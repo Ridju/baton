ba74736e21a0576bcf128f9cd0776206d2031bb7
Ridju <gitmail2@protonmail.com>
2026-08-01 21:34:54 +0200

    added Parser mod

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod scanner;
+mod parser;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/parser.rs
@@ -0,0 +1,28 @@
+
+#[derive(Debug, PartialEq)]
+enum Type {
+    Int,
+}
+
+#[derive(Debug, PartialEq)]
+struct Parameter { 
+    name: String, 
+    param_typ: Type,
+}
+
+#[derive(Debug, PartialEq)]
+struct FunctionDeclData {
+    name: String, 
+    return_type: Type, 
+    parameter: Vec<Parameter>,
+    body: Box<AstNode>, 
+}
+
+#[derive(Debug, PartialEq)]
+enum AstNode {
+    Programm(Vec<AstNode>),
+    FunctionDecl(FunctionDeclData),
+    ReturnStatement(Box<AstNode>),
+    IntLiteralExpr(i16),
+    BlockStatement(Vec<AstNode>),
+}
